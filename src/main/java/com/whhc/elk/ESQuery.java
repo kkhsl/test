7631fee773824086d0bd07bfa690f28b1d68b2c8
15838152042@189.cn <15838152042@189.cn>
2024-10-18 10:18:09 +0800

    2024/10/18/update

--- src/main/java/com/whhc/elk/ESQuery.java
+++ src/main/java/com/whhc/elk/ESQuery.java
@@ -1,6 +1,7 @@
 package com.whhc.elk;
 
-
+import cn.hutool.core.date.DateTime;
+import com.whhc.elk.EsClientConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
@@ -15,22 +16,17 @@ import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Slf4j
-@Service
-public class EsQuery {
+public class ESQuery {
 
-    @Autowired
-    private EsClientConfig esClientConfig;
-
-    public static void test() {
-        String indName = "nat-2024.09.16";
+    @Test
+    public void test() {
+        String indName = "demo-2024.10.17";
 
         EsClientConfig esClientConfig = new EsClientConfig();
 
@@ -82,13 +78,9 @@ public class EsQuery {
         SearchRequest q = new SearchRequest();
     }
 
-    public static void main(String[] args) {
-        EsQuery.test();
-    }
-
     @Test
     public void test2() {
-        String indName = "demo-2024.10.15";
+        String indName = "demo-2024.10.17";
 
         EsClientConfig esClientConfig = new EsClientConfig();
 
@@ -107,12 +99,12 @@ public class EsQuery {
         // 全量查询
         MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
 
-        searchSourceBuilder.query(matchAllQueryBuilder);
+//        searchSourceBuilder.query(matchAllQueryBuilder);
 
         // 聚合查询，按源IP分组查询
-//        AggregationBuilder aggregationBuilder = AggregationBuilders.terms("destIpGroup").field("destIp.keyword");
-
-//        searchSourceBuilder.aggregation(aggregationBuilder);
+        AggregationBuilder aggregationBuilder = AggregationBuilders.terms("destIpGroup").field("destIp.keyword");
+//
+        searchSourceBuilder.aggregation(aggregationBuilder);
 
 
         searchRequest.source(searchSourceBuilder);
@@ -130,6 +122,24 @@ public class EsQuery {
             }
 
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+            //map:key=分组的状态,value=每组的个数
+            Map<String, Integer> stateCountMap = new LinkedHashMap<>();
+            // 获取前 5 个请求最多的源IP
+            Terms terms = response.getAggregations().get("destIpGroup");
+            for (int i = 0; i < 10; i++) {
+                Terms.Bucket bucket = terms.getBuckets().get(i);
+                stateCountMap.put(bucket.getKeyAsString(), Long.valueOf(bucket.getDocCount()).intValue());
+            }
+            stateCountMap.forEach((k, v) -> {
+                System.out.println("destIp = " + k + "，请求次数 = " + v);
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
